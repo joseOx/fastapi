7616d4c003eabc670303a02ea221e98f63461048
Mazaius <mathias.avalos@blueocean.com.py>
2026-04-21 09:41:22 -0300

    crud aplicado logica de endpoints separada de la logica de sqlalchemy

---
+++ app/crud.py
@@ -0,0 +1,31 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+def get_tasks(db: Session):
+    return db.query(models.Task).all()
+
+def get_task_by_id(db: Session, task_id: int):
+    return db.query(models.Task).filter(models.Task.id == task_id).first()
+
+def create_task(db: Session, task: schemas.TaskCreate):
+    db_task = models.Task(**task.model_dump())
+    db.add(db_task)
+    db.commit()
+    db.refresh(db_task)
+    return db_task
+
+def update_task(db: Session, task_id: int, task_data: schemas.TaskUpdate):
+    db_task = get_task_by_id(db, task_id)
+    if db_task:
+        for key, value in task_data.model_dump().items():
+            setattr(db_task, key, value)
+        db.commit()
+        db.refresh(db_task)
+    return db_task
+
+def delete_task(db: Session, task_id: int):
+    db_task = get_task_by_id(db, task_id)
+    if db_task:
+        db.delete(db_task)
+        db.commit()
+    return db_task
--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -2,9 +2,8 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
 from ..database import get_db
-from .. import models, schemas
+from .. import schemas, crud  # Importamos crud
 
-#Creo el router
 router = APIRouter(
     prefix="/tasks",
     tags=["Tasks"]
@@ -12,13 +11,29 @@ router = APIRouter(
 
 @router.get("/", response_model=List[schemas.Task])
 async def read_tasks(db: Session = Depends(get_db)):
-    return db.query(models.Task).all()
+    return crud.get_tasks(db)
 
 @router.post("/", response_model=schemas.Task)
 async def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
-    new_task = models.Task(**task.model_dump())
+    return crud.create_task(db=db, task=task)
 
-    db.add(new_task)
-    db.commit()
-    db.refresh(new_task)
-    return new_task
+@router.get("/{task_id}", response_model=schemas.Task)
+async def read_task(task_id: int, db: Session = Depends(get_db)):
+    db_task = crud.get_task_by_id(db, task_id=task_id)
+    if db_task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return db_task
+
+@router.put("/{task_id}", response_model=schemas.Task)
+async def update_task(task_id: int, task: schemas.TaskCreate, db: Session = Depends(get_db)):
+    db_task = crud.update_task(db, task_id, task)
+    if db_task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return db_task
+
+@router.delete("/{task_id}")
+async def delete_task(task_id: int, db: Session = Depends(get_db)):
+    success = crud.delete_task(db, task_id)
+    if not success:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return {"message": "Task deleted successfully"}
